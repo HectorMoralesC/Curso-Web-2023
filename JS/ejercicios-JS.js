ec70dbc452ef6dd403d6e8fff2b104d24345fe4b
HectorMoralesC <hectormoralescortes6@gmail.com>
2023-03-20 18:20:19 +0100

    20-03-23

--- JS/ejercicios-JS.js
+++ JS/ejercicios-JS.js
@@ -9,13 +9,110 @@ document.write(numero);
 var CMS = "WordPress";
 document.getElementById("cms1").innerHTML = CMS;
 
+//Ejercicio 3
 var temperatura = "Frio";
 document.getElementById("temp").innerHTML = temperatura;
 
 
-//Ejercicio 3
-
+//Ejercicio 4
 var x, y;
 x = 2
 y = 3
 document.getElementById("sum").innerHTML ="2 + 3 = "+ (x+y);
+
+
+
+//Ejercicio 5
+var z;
+
+z = x + y;
+document.getElementById("z").innerHTML ="z = "+ (z);
+
+
+//Ejercicio 6
+var persona = ["Cristina", "García", "39"];
+
+var nombre = persona [0]; 
+var apellido = persona [1]; 
+var edad = persona [2]; 
+document.getElementById("persona").innerHTML = nombre + " " + apellido + " tiene " + edad + " años";
+
+
+//Ejercicio 7
+x = 1;
+
+y = 6;
+
+z = x + y;
+
+document.getElementById("donde").innerHTML = z;
+
+
+//Ejercicio 8
+x = 60;
+
+document.getElementById("division").innerHTML = (x/3);
+
+
+//Ejercicio 9
+x = 6;
+y = 9;
+document.getElementById("multiplicacion").innerHTML = (x*y);
+
+
+
+//Ejercicio 10
+//Ejercicio 11
+//Ejercicio 11
+
+
+
+
+//Ejercicio 13
+var txt = "Hola Mundo!";
+
+document.getElementById("txt.length").innerHTML =
+txt.length;
+
+
+//Ejercicio 14
+
+
+
+//Ejercicio 15
+
+var str1 = "¡Hola ";
+
+var str2 = "mundo!";
+document.getElementById("concatenar").innerHTML = (str1+""+str2);
+
+
+//Ejercicio 16
+var txt = document.getElementById("concatenar").innerHTML = (str1+""+str2);
+document.getElementById("txt").innerHTML = (txt);
+
+
+//Ejercicio 17
+random = Math.random(Math.random()*10000);
+document.getElementById("random").innerHTML = "Número random = " + random;
+
+
+
+
+
+
+
+//Ejercicio 27 sin acabar
+
+
+function myFunction() {
+
+    document.getElementById("ejercicio27").innerHTML = "Hello World!";
+    
+    }
+    
+    
+
+   
+    
+   
