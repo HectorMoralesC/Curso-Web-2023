238118be683a798dd7612792ad19275959514196
HectorMoralesC <hectormoralescortes6@gmail.com>
2023-03-23 18:18:06 +0100

    23-03-23

---
+++ JS/eventos.js
@@ -0,0 +1,39 @@
+function displayDate() {
+    document.getElementById("BotonEvento").innerHTML = Date();
+    
+  }
+
+
+function ejercicio(){
+
+    document.getElementById("ejercicio").innerHTML = "fofkofkokoeokfkooeokoooooooooooooooooooooooooooooooooooooooooooooooooooooooooo"
+
+}
+
+function ejercicio1(){
+
+    document.getElementById("ejercicio").innerHTML = ""
+    document.getElementById("doubleclick").innerHTML = ""
+
+}
+
+function onkeydown(){
+
+    document.getElementById("onkeydown").innerHTML = "efkfekefkefk"
+
+}
+
+function doubleclick(){
+
+    document.getElementById("doubleclick").innerHTML = "fofkofkokoeokfkooeokoooooooooooooooooooooooooooooooooooooooooooooooooooooooooo"
+
+}
+
+
+function rueda() {
+    document.getElementById("ruedaraton").innerHTML = "Has hecho scroll!!!";
+    
+}
+
+
+
