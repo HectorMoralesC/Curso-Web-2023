7d42bc414f513b80963b971f94842b1e677a31b0
HectorMoralesC <hectormoralescortes6@gmail.com>
2023-04-12 16:56:44 +0200

    Exámen Héctor

---
+++ JS/comportament.js
@@ -0,0 +1,18 @@
+//Párrafo cambiado
+document.getElementById("canvia").innerHTML =("Paràgraf canviat");
+
+//Función 1
+function funcion1 (){
+    document.getElementById("paragraf").style.fontSize = "35px";
+}
+//Función 2
+function funcion2 (){
+    document.getElementById("titol").style.color = "red";
+   
+}
+//Función 3
+function funcion3 (){
+
+    document.getElementById("canvia").innerHTML = "Exercici 5 resolt"
+
+}
