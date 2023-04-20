7f9d591edc73b2345adc7c31a813b9ec233c788c
HectorMoralesC <hectormoralescortes6@gmail.com>
2023-04-20 19:35:42 +0200

    Examen 3

---
+++ JS/web.js
@@ -0,0 +1,9 @@
+//Función 1
+function agranda (){
+    document.getElementById("paragraf").style.fontSize = "15pt";
+}
+
+//Función 2
+function empequeñece (){
+    document.getElementById("paragraf").style.fontSize = "8pt";
+}
